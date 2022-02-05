c183aed3b5a63d94f1d81bda7db8dc10ba88af35
SurajThakur4995 <89120433+SurajThakur4995@users.noreply.github.com>
2022-02-05 14:47:20 +0530

    Add files via upload

---
+++ src/app/admin-home-page/admin-home-page-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AdminHomePageComponent } from './admin-home-page.component';
+
+const routes: Routes = [{ path: '', component: AdminHomePageComponent }];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AdminHomePageRoutingModule { }
